cc609389029ee989518cbf8ec6ad6de45743d0b7
vedatmertbal <60327995+vedatmertbal@users.noreply.github.com>
2020-11-01 15:25:53 +0300

    Create turkiye_sehir_havadurumu.py

---
+++ turkiye_sehir_havadurumu.py
@@ -0,0 +1,165 @@
+print("Herkese merhaba bugün size hava durumlarını açıklayacağız.")
+print("Not: Kıbrıs için ""CY yazınız.")
+a1=int(input("Yaşadığınız şehrin plaka numarasını giriniz."))
+if a1 == 1:
+    print("Adana'da bugün hava çok sıcak.")
+if a1 == 2:
+    print("Adıyaman'da hava çok sıcak ama esen bir rüzgar daha var ")
+if a1 == 3:
+    print("Afyon'da bugün havalar çok sıcak")
+if a1 == 4:
+    print("Ağrı'da havalar sisli.")
+if a1 == 5:
+    print("Amasya'da havalar soğuk.")
+if a1 == 6:
+    print("Ankara soğuk bir durumda, KAr yağışı bekleniyor.")
+if a1== 7:
+    print("Antalya, her zaman olduğu gibi sıcak.")
+if a1 == 8:
+    print("Artvin, ılık bir şekilde yağmur bekleniyor.")
+if a1 == 10:
+    print("Balıkesir çok sıcak denize girilebilir.")
+if a1 == 11:
+    print("Bilecik'de havalar çok sıcak.")
+if a1 == 12:
+    print("Bingöl'de havalar çok soğuk birkaç saate kar yağışı bekleniyor.")
+if a1 == 13:
+    print("Bitlis'de havalar sisli.")
+if a1 == 14:
+    print("Bolu'da havalar sıcak")
+if a1 == 15:
+    print("Burdur'da havalar soğuk.")
+if a1 == 16:
+    print("Bursa'da havalar Bolu gibi sıcak.")
+if a1 == 17:
+    print("Çanakkale'de havalar çok güzel deniz ısısı 18 C")
+if a1 == 18:
+    print("Çankırı'da hava çok soğuk montunuzu giyip dışarıya çıkabilirsiniz.")
+if a1 == 19:
+    print("Çorum'da havalar ılık.")
+if a1 == 20:
+    print("Denizli'de havalar çok iyi.")
+if a1 == 21:
+    print("Diyarbakır'da havalar çok soğuk kar yağışı bekliyoruz.")
+if a1 == 22:
+    print("Edirne'de halkın deyimiyle bir eşek donduran soğuğu söz konusu. ")
+if a1 == 23:
+    print("Elazığ çok sıcak başınıza güneş geçebilir.")
+if a1 == 24:
+    print("Erzincan çok soğuk.")
+if a1 == 25:
+    print("Erzurum, çok sıcak.")
+if a1 == 26:
+    print("Eskişehir çok sıcak olmasa da sıcak.")
+if a1 == 27:
+     print("Gaziantep bugün Türkiye'nin en sıcak ili.")
+if a1 == 28:
+    print("Giresun bugün ılık.")
+if a1 == 29:
+    print("Gümüşhane sıcak.")
+if a1 == 30:
+    print("Hakkari çok soğuk.")
+if a1 == 31:
+    print("Hatay çok sıcak.")
+if a1 == 32:
+    print("Isparta ılık.")
+if a1 == 33:
+    print("Mersin çok sıcak.")
+if a1 == 34:
+    print("İstanbul ılık bir sıcaklıkta.")
+if a1 == 35:
+    print("İzmir çok sıcak Seferhisar depreminde zarar gören herkesin Allah yardımcısı olsun.")
+if a1 == 36:
+    print("Kars çok sıcak.")
+if a1 == "CY":
+    print("Lefkoşa çok sıcak.")
+if a1 == 37:
+    print("Kastamonu bugün sıcak.")
+if a1 == 38:
+    print("Kayseri'de hava çok güzel, çok ılık")
+if a1 == 39:
+    print("Kırklareli 18C ılık.")
+if a1 == 40:
+    print("Kırşehir bugün çok soğuk")
+if a1 == 41:
+    print("Kocaeli bugün çok sıcak.")
+if a1 == 42:
+    print("Konya çok sıcak.")
+if a1 == 43:
+    print("Kütahya çok sıcak")
+if a1 == 44:
+    print("Malatya ılık.")
+if a1 == 45:
+    print("Manisa bugün çok sıcak")
+if a1 == 46:
+    print("K.Maraş bugün ılık")
+if a1 == 47:
+    print("Mardin bugün çok sıcak")
+if a1 == 48:
+    print("Muğla en sıcak şehirlerimizden biri 38c")
+if a1 == 49:
+    print("Muş bugün ılık")
+if a1 == 50:
+    print("Nevşehir bugün sıcak")
+if a1 == 51:
+    print("Niğde bugün çok sıcak")
+if a1 == 52:
+    print("Ordu her zaman ki gibi sıcak :)")
+if a1 == 53:
+    print("Rize bugün çok sıcak")
+if a1 == 54:
+    print("Sakarya bugün çok soğuk")
+if a1 == 55:
+    print("Samsun her zaman olduğu gibi yine sıcak.")
+if a1 == 56:
+    print("Siirt bugün çok sıcak")
+if a1 == 57:
+    print("Sinop biraz soğuk. ")
+if a1 == 58:
+    print("Sivas bugün soğuk.")
+if a1 == 59:
+    print("Tekirdağ serin.")
+if a1 == 60:
+    print("Tokat çok sıcak.")
+if a1 == 61:
+    print("Trabzon yağmurlu.")
+if a1 == 62:
+    print("Tunceli her zamanki gibi sıcak.")
+if a1 == 63:
+    print("Şanlıurfa yine sıcak.")
+if a1 == 64:
+    print("Uşak sıcak.")
+if a1 == 65:
+    print("Van çok olmasa da soğuk.")
+if a1 == 66:
+    print("Yozgat ılımlı bir havaya sahip.")
+if a1 == 67:
+    print("Zonguldak çok sıcak")
+if a1 == 68:
+    print("Aksaray ılık.")
+if a1 == 69:
+    print("Bayburt soğuk")
+if a1 == 70:
+    print("Karaman soğuk.")
+if a1 == 71:
+    print("Kırıkkale soğuk.")
+if a1 == 72:
+    print("Batman soğuk.")
+if a1 == 73:
+    print("Şırnak sıcak.")
+if a1 == 74:
+    print("Bartın çok sıcak")
+if a1 == 75:
+    print("Ardahan çok soğuk")
+if a1 == 76:
+    print("Iğdır çok soğuk")
+if a1 == 77:
+    print("Yalova yine sıcak.")
+if a1 == 78:
+    print("Karabük yine çok soğuk")
+if a1 == 79:
+    print("Kilis bugün çok soğuk")
+if a1 == 80:
+    print("Osmaniye bugün çok soğuk")
+if a1 == 81:
+    print("Düzce bugün ılımlı bir havaya sahip.")
